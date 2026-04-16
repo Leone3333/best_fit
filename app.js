9f967846f053d957d6282e00d07e188cdb701385
leone3333 <leonegoveia1@gmail.com>
2026-04-16 15:49:23 -0300

    Controller login e logout completo

--- app.js
+++ app.js
@@ -5,15 +5,22 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
   secret: 'seu_segredo_aqui', // Uma frase aleatória para criptografar o ID da sessão
@@ -23,14 +30,16 @@ app.use(session({
     maxAge: 1000 * 60 * 60 * 24 // Sessão dura 24 horas
   }
 }));
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use((req,res,next) => {
+  res.locals.usuario = req.session.usuarioLogado || null;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,23 @@ router.get('/', function (req, res, next) {
 });
 
 
+router.get('/logout', function (req, res, next) {
+  // 1. Destrói a sessão no servidor
+  req.session.destroy((err) => {
+    if (err) {
+      console.log("Erro ao destruir sessão:", err);
+      return res.redirect('/home'); // Ou para onde desejar em caso de erro
+    }
+    
+    // 2. Limpa o cookie do navegador (Opcional, mas boa prática)
+    res.clearCookie('connect.sid'); 
+
+    // 3. Redireciona para a rota de login (A URL vai mudar)
+    res.redirect('/'); 
+  });
+});
+
+
 router.post('/home', async (req, res, next) => {
   const { email, password } = req.body;
 
@@ -20,13 +37,18 @@ router.post('/home', async (req, res, next) => {
         id: user.idusuario,
         nome: user.nome,
       };
-  
-      res.render('home', { user: user[0] })
-  
+
+      console.log("Sessão criada:", req.session.usuarioLogado);
+
+      req.session.save(() => {
+
+        res.render('home', { usuario: req.session.usuarioLogado });
+      })
+
     } else {
-      res.render('login', { user: "error" })
-    }
-    
+      res.render('login', { user: "error" });
+    };
+
   } catch (error) {
     console.log("Erro controller")
   }
